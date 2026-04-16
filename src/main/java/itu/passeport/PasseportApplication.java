3e70df33a47ebe2dc0a0f9e22615cd0c7be2eb47
Mazà Faniriniaina <faniryranaivoson167@gmail.com>
2026-04-16 14:52:53 +0300

    Init

---
+++ src/main/java/itu/passeport/PasseportApplication.java
@@ -0,0 +1,13 @@
+package itu.passeport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasseportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasseportApplication.class, args);
+    }
+
+}
