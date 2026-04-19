af89ad1ce27bce32123446096f23789394558310
eXen54 <168262555+eXen54@users.noreply.github.com>
2026-04-19 18:57:53 +0300

    Implement dynamic form population for visa types and add repositories for pieces and fields

---
+++ src/main/java/itu/passeport/repositories/ReferenceChampTypeVisaRepository.java
@@ -0,0 +1,9 @@
+package itu.passeport.repositories;
+
+import itu.passeport.entities.ReferenceChampTypeVisa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReferenceChampTypeVisaRepository extends JpaRepository<ReferenceChampTypeVisa, Integer> {
+    List<ReferenceChampTypeVisa> findByTypeVisaId(Integer typeVisaId);
+}
