a19b3a71a6707095882d62d6cc21a0bd477f912d
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2026-05-12 23:44:19 +0300

    chore: add diagnostic logs and documentation for build and compilation errors

--- passeport-spring-boot/src/main/java/itu/passeport/constante/StatusConstante.java
+++ passeport-spring-boot/src/main/java/itu/passeport/constante/StatusConstante.java
@@ -2,8 +2,9 @@ package itu.passeport.constante;
 
 public final class StatusConstante {
     public static final int DEMANDE_CREE = 1;
-    public static final int DEMANDE_APPROUVE = 3;
-    //public static final int DEMANDE_EN_ATTENTE = 2;
+    public static final int DEMANDE_PHOTO_TERMINEE = 2;
+    public static final int DEMANDE_SCAN_TERMINE = 3;
+    public static final int DEMANDE_APPROUVE = 4;
 
     private StatusConstante() {
     }
